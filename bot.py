b774cdcaa2f3c777399b7afad6f7c85772484358
thaw-1999 <khaingzar203333@gmail.com>
2026-06-11 22:32:39 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKe
 from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
-from fragment_api_lib.client import FragmentAPIClient
+
 
 # ==================== .env ဖိုင်ကို ဖတ်ရန် နေရာ ====================
 try:
